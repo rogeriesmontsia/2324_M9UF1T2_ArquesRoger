01f25650322a2b52de0c4237b2fb167cb1ade239
rogeriesmontsia <rogerarques@iesmontsia.org>
2023-10-10 16:33:37 +0200

    add bootstrap styles

--- includes/functions.php
+++ includes/functions.php
@@ -15,19 +15,31 @@ function mostrarHistorial()
   // Itera a través del historial en ordre invers
   for ($i = $total_operacions - 1; $i >= 0; $i--) {
     $operacio = $_SESSION['history'][$i];
-    echo "$operacio<br><hr>";
+    echo "$operacio<br><hr class=\"border border-primary border-1 opacity-75\">";
   }
 }
 
 function mostrarResultat($result)
 {
-  echo "<div class=\"caixaResultat\"><h2>🥁Resultat🥁</h2>";
-  echo "<h1>$result</h1></div>";
+  echo "<div class=\"container mt-0 text-center\">";
+  echo "<div class=\"row justify-content-center\">";
+  echo "<div class=\"col-10 col-md-8 col-lg-6 badge bg-warning-subtle text-center p-3 rounded text-black text-wrap\">";
+  echo "<h2>🥁 Resultat 🥁</h2>";
+  echo "<h1>$result</h1>";
+  echo "</div>";
+  echo "</div>";
+  echo "</div>";
 }
 
 function mostrarAlerta($message)
 {
-  echo "<div class=\"caixaAlerta\">$message</div><br>";
+  //echo "<div class=\"container mt-0 border border-secondary\">";
+  //echo "<div class=\"row justify-content-center align-items-center border border-secondary\">";
+  echo "<div class=\"alert alert-danger text-center\" role=\"alert\">";
+  echo $message;
+  echo "</div";
+  //echo "</div";
+  //echo "</div";
 }
 
 function llimpiarHistorial()
--- index.php
+++ index.php
@@ -55,57 +55,55 @@ if (isset($_POST['llimpiar_historial'])) {
   <title>Calculadora🧙‍♂️</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-</head>
 
-<header>
+</head>
+<header class="bg-primary fs-1 fw-bold bg-gradient py-3 text-center">
   <h1>Calculadora🧙‍♂️</h1>
 </header>
 
-<body>
-  <div class="container mt-5">
-    <div class="row">
-      <div class="col-md-6 offset-md-3">
+<body class="bg-info bg-gradient">
+  <!--Seccio per al formulari-->
+  <section class="container mt-0">
+    <div class="row justify-content-center">
+      <div class="text-center col-10 col-sm-10 col-md-7 col-lg-6 col-xl-5 p-5"> 
         <form class="formulariInputs" method="POST">
-        <div class="form-group">
-          <input type="text" id="input1" name="input1" placeholder="Primer valor"><br>
+          <input type="text" id="input1" name="input1" class="form-control" placeholder="Primer valor">
           <label for="operacio">Operació a realitzar:</label><br>
-        </div>
-        <div class="form-check">
-            <input type="radio" id="suma" name="operacio" value="+" checked="checked">
-            <label for="operacio">➕</label><br>
-        </div>
-        <div class="form-check">
-            <input type="radio" id="resta" name="operacio" value="-">
-            <label for="operacio">➖</label><br>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" id="suma" name="operacio" value="+" class="form-check-input" checked>
+            <label for="operacio" class="form-check-label pl-2">➕</label>
           </div>
-          <div class="radio-option">
-            <input type="radio" id="multiplicacio" name="operacio" value="*">
-            <label for="operacio">✖️</label><br>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" id="resta" name="operacio" value="-" class="form-check-input">
+            <label for="operacio" class="form-check-label pl-2">➖</label>
           </div>
-          <div class="radio-option">
-            <input type="radio" id="divisio" name="operacio" value="/">
-            <label for="operacio">➗</label><br>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" id="multiplicacio" name="operacio" value="*" class="form-check-input">
+            <label for="operacio" class="form-check-label pl-2">✖️</label>
           </div>
-          <div class="radio-option">
-            <input type="radio" id="factorial" name="operacio" value="factorial">
-            <label for="operacio">❗</label><br>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" id="divisio" name="operacio" value="/" class="form-check-input">
+            <label for="operacio" class="form-check-label pl-2">➗</label>
           </div>
-          <div class="radio-option">
-            <input type="radio" id="concatena" name="operacio" value="concatenate">
-            <label for="operacio">Concatenar strings</label><br>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" id="factorial" name="operacio" value="factorial" class="form-check-input">
+            <label for="operacio" class="form-check-label pl-2">❗</label>
           </div>
-          <div class="radio-option">
-            <input type="radio" id="elimina" name="operacio" value="eliminar">
-            <label for="operacio">Eliminar part string</label><br><br>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" id="concatena" name="operacio" value="concatena" class="form-check-input">
+            <label for="operacio" class="form-check-label pl-2">Concatenar strings</label>
           </div>
-          <input type="text" id="input2" name="input2" placeholder="Segon valor"><br>
-
-          <button type="submit" class="btn btn-primary" name="calcular">Calcular🪄</button>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" id="elimina" name="operacio" value="eliminar" class="form-check-input">
+            <label for="operacio" class="form-check-label pl-2">Eliminar part string</label>
+          </div>
+          <input type="text" id="input2" name="input2" class="form-control" placeholder="Segon valor">
+          <button type="submit" class="form-control btn btn-primary mt-3" name="calcular">Calcular🪄</button>
         </form>
       </div>
     </div>
-  </div>
-
+  </section>
+  <!--Seccio per a l'ALERTA-->
   <?php
   if (isset($_POST['calcular']) && ($result != 'Operació no vàlida')) {
     mostrarResultat($result);
@@ -116,23 +114,25 @@ if (isset($_POST['llimpiar_historial'])) {
   }
   ?>
 
-  <div class="historial">
-    <h1>📜</h1>
-    <h2>Historial d'operacions</h2>
-    <h1>📜</h1>
-    <form class="formBoto" method="POST">
-      <button class="botoEliminar" type="submit" name="llimpiar_historial" title="Eliminar historial">
-        <h1>🗑️</h1>
-      </button>
-    </form>
-  </div>
+  <!--Seccio per a l'historial-->
+  <section class="container-fluid mt-0">
+    <section class="d-flex justify-content-center align-items-center p-3">
+      <h2>📜Historial d'operacions📜</h2>
+      <form method="POST">
+        <button class="botoEliminar btn" type="submit" name="llimpiar_historial" title="Eliminar historial">
+          <h1>🗑️</h1>
+        </button>
+      </form>
+    </section>
+  </section>
 
-  <div id="history">
-    <?php
-    mostrarHistorial();
+
+  <div class="container-fluid">
+  <div class="row justify-content-center align-items-center p-3">
+    <?php 
+    mostrarHistorial(); 
     ?>
   </div>
-
+  </div>
 </body>
-
 </html>
